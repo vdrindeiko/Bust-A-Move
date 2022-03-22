f9c0a992662f16aeaee1d8259c72b4487f47e7d0
Niko Pelletier <npelletier3435@gmail.com>
2022-03-21 20:51:22 -0400

    Niko's A05 done

--- 2900/A09/member1/game.js
+++ 2900/A09/member1/game.js
@@ -1,320 +1,287 @@
-// game.js for Perlenspiel 3.3
-// The following comment lines are for JSHint. You can remove them if you don't use JSHint.
+//Niko Pelletier
+//Bust-A-Move
+//Mod 1: The grid size can be changed by using the arrow keys. Left and right change 
+//		 the width, and down and up change the height.
+//Mod 2: Q and W can be used to cycle through different colors, which will be used to
+//		 color a bead that is clicked on.
+//Mod 3: Press B to toggle bead border visibility.
+//Mod 4: Press C to toggle between a light or dark background.
+//Mod 5: Changed sound effect to fx_drip2.
+
+/*
+game.js for Perlenspiel 3.3.x
+Last revision: 2022-03-15 (BM)
+
 /* jshint browser : true, devel : true, esversion : 6, freeze : true */
 /* globals PS : true */
 
-"use strict";
+"use strict"; // Do NOT remove this directive!
 
-// The G object will contain all public constants, variables and functions.
-// The immediately invoked function expression (IIFE) encapsulates all game functionality.
-// It is called as this file is loaded, and initializes the G object.
+const G = (function(){
+	//Used for color switching capability
+	const colorList = [PS.COLOR_BLACK, PS.COLOR_GRAY, PS.COLOR_WHITE,
+					   PS.COLOR_RED, PS.COLOR_ORANGE, PS.COLOR_YELLOW,
+					   PS.COLOR_GREEN, PS.COLOR_CYAN, PS.COLOR_BLUE,
+					   PS.COLOR_INDIGO, PS.COLOR_VIOLET, PS.COLOR_MAGENTA];
+	const colorStrs = ["Black", "Gray", "White",
+					   "Red", "Orange", "Yellow",
+					   "Green", "Cyan", "Blue",
+					   "Indigo", "Violet", "Magenta"];
+	
+	let colorIndex = 0;
+	
+	const setStatusText = function(){
+		PS.statusText("Selected color: " + colorStrs[colorIndex]);
+	}
 
-const G = ( function () {
-
-	// Constants are in all upper-case
-
-	const WIDTH = 21; // grid width
-	const HEIGHT = 21; // grid height
-
-	const PLANE_FLOOR = 0; // z-plane of floor
-	const PLANE_ACTOR = 1; // z-plane of actor
-
-	const COLOR_BG = PS.COLOR_GRAY_DARK; // background color
-	const COLOR_WALL = PS.COLOR_BLACK; // wall color
-	const COLOR_FLOOR = PS.COLOR_GRAY; // floor color
-	const COLOR_ACTOR = PS.COLOR_GREEN; // actor color
-	const COLOR_GOLD = PS.COLOR_YELLOW; // gold color
-	const COLOR_EXIT = PS.COLOR_BLUE; // exit color
-
-	const SOUND_FLOOR = "fx_click"; // touch floor sound
-	const SOUND_WALL = "fx_hoot"; // touch wall sound
-	const SOUND_GOLD = "fx_coin1"; // take coin sound
-	const SOUND_OPEN = "fx_powerup8"; // open exit sound
-	const SOUND_WIN = "fx_tada"; // win sound
-	const SOUND_ERROR = "fx_uhoh"; // error sound
-
-	const WALL = 0; // wall
-	const FLOOR = 1; // floor
-	const GOLD = 2; // floor + gold
-
-	// Variables
-
-	let id_sprite; // actor sprite id
-	let id_path; // pathmap id for pathfinder
-	let id_timer; // timer id
-
-	let gold_count; // initial number of gold pieces in map
-	let gold_found; // gold pieces collected
-	let won = false; // true on win
-
-	// This handmade imageMap is used for map drawing and pathfinder logic
-	// All properties MUST be present!
-	// The map.data array controls the layout of the maze,
-	// the location of the gold pieces and exit
-	// 0 = wall, 1 = floor, 2 = floor + gold
-	// To remove a gold piece, replace a 2 with a 1
-	// To add a gold piece, replace a 1 with a 2
-
-	let map = {
-		width: WIDTH, // must match WIDTH!
-		height: HEIGHT, // must match HEIGHT!
-		pixelSize: 1, // must be present!
-		data: [
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-			0, 1, 1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0,
-			0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0,
-			0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 2, 1, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-			0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-			0, 1, 0, 0, 1, 2, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0,
-			0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 2, 0,
-			0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0,
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-		]
-	};
-
-	// These two variables control the initial location of the actor
-	// This location MUST correspond to a floor location (1) in the maza.data array
-	// or a startup error will occur!
-
-	let actorX = 1; // initial x-pos of actor sprite
-	let actorY = 1; // initial y-pos of actor sprite
-
-	// These two variables control the location of the exit
-	// This location MUST correspond to a floor location (1) in the maza.data array
-	// or a startup error will occur!
-
-	let exitX = 19; // x-pos of exit
-	let exitY = 19; // y-pos of exit
-	let exit_ready = false; // true when exit is opened
-
-	// Timer function, called every 1/10th sec
-	// This moves the actor along paths
-
-	let path; // path to follow, null if none
-	let step; // current step on path
-
-	const tick = function () {
-		let p, nx, ny, ptr, val;
-
-		if ( !path ) { // path invalid (null)?
-			return; // just exit
+	const getColor = function(){return colorList[colorIndex]}
+	const nextColor = function(){
+		colorIndex = (colorIndex + 1) % colorList.length;
+		setStatusText();
+		return colorList[colorIndex];
+	}
+	const prevColor = function(){
+		colorIndex--;
+		if(colorIndex < 0){
+			colorIndex = colorList.length - 1;
 		}
+		setStatusText();
+		return colorList[colorIndex];
+	}
 
-		// Get next point on path
-
-		p = path[ step ];
-		nx = p[ 0 ]; // next x-pos
-		ny = p[ 1 ]; // next y-pos
-
-		// If actor already at next pos,
-		// path is exhausted, so nuke it
-
-		if ( ( actorX === nx ) && ( actorY === ny ) ) {
-			path = null;
-			return;
+	//Border toggling
+	let borderVisible = true;
+	const setBorderVisibility = function(){
+		if(borderVisible){
+			PS.border(PS.ALL, PS.ALL, 1);
+		}else{
+			PS.border(PS.ALL, PS.ALL, 0);
 		}
+	}
+	const toggleBorderVisibility = function(){
+		borderVisible = !borderVisible;
+		setBorderVisibility();
+	}
 
-		// Move sprite to next position
-
-		PS.spriteMove( id_sprite, nx, ny );
-		actorX = nx; // update actor's xpos
-		actorY = ny; // and ypos
-
-		// If actor has reached a gold piece, take it
-
-		ptr = ( actorY * HEIGHT ) + actorX; // pointer to map data under actor
-		val = map.data[ ptr ]; // get map data
-		if ( val === GOLD ) {
-			map.data[ ptr ] = FLOOR; // change gold to floor in map.data
-			PS.gridPlane( PLANE_FLOOR ); // switch to floor plane
-			PS.color( actorX, actorY, COLOR_FLOOR ); // change visible floor color
-
-			// If last gold has been collected, activate the exit
-
-			gold_found += 1; // update gold count
-			if ( gold_found >= gold_count ) {
-				exit_ready = true;
-				PS.color( exitX, exitY, COLOR_EXIT ); // show the exit
-				PS.glyphColor( exitX, exitY, PS.COLOR_WHITE ); // mark with white X
-				PS.glyph( exitX, exitY, "X" );
-				PS.statusText( "Found " + gold_found + " gold! Exit open!" );
-				PS.audioPlay( SOUND_OPEN );
-			}
-
-			// Otherwise just update score
-
-			else {
-				PS.statusText( "Found " + gold_found + " gold!" );
-				PS.audioPlay( SOUND_GOLD );
-			}
+	//Background toggling
+	let darkBG = true;
+	const setBG = function(){
+		if(darkBG){
+			PS.gridColor(0x303030);
+			PS.statusColor(PS.COLOR_WHITE);
+		}else{
+			PS.gridColor(PS.COLOR_WHITE);
+			PS.statusColor(PS.COLOR_BLACK);
 		}
-
-		// If exit is ready and actor has reached it, end game
-
-		else if ( exit_ready && ( actorX === exitX ) && ( actorY === exitY ) ) {
-			PS.timerStop( id_timer ); // stop movement timer
-			PS.statusText( "You escaped with " + gold_found + " gold!" );
-			PS.audioPlay( SOUND_WIN );
-			won = true;
-			return;
-		}
-
-		step += 1; // point to next step
-
-		// If no more steps, nuke path
-
-		if ( step >= path.length ) {
-			path = null;
-		}
-	};
-
-	// Public functions are exposed in the global G object, which is initialized here.
-	// Only two functions need to be exposed; everything else is encapsulated!
-	// So safe. So elegant.
+	}
+	const toggleBG = function(){
+		darkBG = !darkBG;
+		setBG();
+	}
 
 	return {
-		// Initialize the game
-		// Called once at startup
-
-		init : function () {
-			let x, y, val, color;
-
-			// Establish grid size
-			// This should always be done FIRST, before any other initialization!
-
-			PS.gridSize( WIDTH, HEIGHT );
-
-			// Check for illegal actor/exit locations
-
-			val = map.data[ ( actorY * HEIGHT ) + actorX ]; // get map data under actor
-			if ( val !== FLOOR ) {
-				PS.debug( "ERROR: Actor not on empty floor!" );
-				PS.audioPlay( SOUND_ERROR );
-				return;
-			}
-
-			val = map.data[ ( exitY * HEIGHT ) + exitX ]; // get map data at exit position
-			if ( val !== FLOOR ) {
-				PS.debug( "ERROR: Exit not on empty floor!" );
-				PS.audioPlay( SOUND_ERROR );
-				return;
-			}
-
-			PS.gridColor( COLOR_BG ); // grid background color
-			PS.border( PS.ALL, PS.ALL, 0 ); // no bead borders
-			PS.statusColor( PS.COLOR_WHITE );
-			PS.statusText( "Click/touch to move" );
-
-			// Use the map.data array to draw the maze
-			// This also counts the number of gold pieces that have been placed
-
-			gold_count = gold_found = 0;
-			for ( y = 0; y < HEIGHT; y += 1 ) {
-				for ( x = 0; x < WIDTH; x += 1 ) {
-					val = map.data[ ( y * HEIGHT ) + x ]; // get data
-					if ( val === WALL ) {
-						color = COLOR_WALL;
-					}
-					else if ( val === FLOOR ) {
-						color = COLOR_FLOOR;
-					}
-					else if ( val === GOLD ) {
-						color = COLOR_GOLD;
-						gold_count += 1; // add to count
-					}
-					PS.color( x, y, color );
-				}
-			}
-
-			// Preload & lock sounds
-
-			PS.audioLoad( SOUND_FLOOR, { lock : true } );
-			PS.audioLoad( SOUND_WALL, { lock : true } );
-			PS.audioLoad( SOUND_GOLD, { lock : true } );
-			PS.audioLoad( SOUND_OPEN, { lock : true } );
-			PS.audioLoad( SOUND_WIN, { lock : true } );
-
-			// Create 1x1 solid sprite for actor
-			// Place on actor plane in initial actor position
-
-			id_sprite = PS.spriteSolid( 1, 1 );
-			PS.spriteSolidColor( id_sprite, COLOR_ACTOR );
-			PS.spritePlane( id_sprite, PLANE_ACTOR );
-			PS.spriteMove( id_sprite, actorX, actorY );
-
-			// Create pathmap from our imageMap
-			// for use by pathfinder
-
-			id_path = PS.pathMap( map );
-
-			// Start the timer function that moves the actor
-			// Run at 10 frames/sec (every 6 ticks)
-
-			path = null; // start with no path
-			step = 0;
-			id_timer = PS.timerStart( 6, tick );
-		},
-
-		// touch( x, y )
-		// Set up new path for the actor to follow.
-		// NOTE: data and options parameters are currently unused.
-
-		touch : function ( x, y ) {
-			let line;
-
-			// Do nothing if game over
-
-			if ( won ) {
-				return;
-			}
-
-			// Use pathfinder to calculate a line from current actor position
-			// to touched position
-
-			line = PS.pathFind( id_path, actorX, actorY, x, y );
-
-			// If line is not empty, it's valid,
-			// so make it the new path
-			// Otherwise hoot at the player
-
-			if ( line.length > 0 ) {
-				path = line;
-				step = 0; // start at beginning
-				PS.audioPlay( SOUND_FLOOR );
-			}
-			else {
-				PS.audioPlay( SOUND_WALL );
-			}
-		}
+		getColor, nextColor, prevColor, setStatusText,
+		setBorderVisibility, toggleBorderVisibility,
+		setBG, toggleBG
 	};
-} () ); // end of IIFE
+} ());
 
-// The following calls assign the G.init() and G.touch() functions above to Perlenspiel's event handlers.
+PS.init = function( system, options ) {
+	// Establish grid dimensions
 
-// PS.init( system, options )
-// Initializes the game
+	PS.gridSize( 7, 7 );
 
-PS.init = function ( system, options ) {
-	G.init(); // game-specific initialization
+	// Set background color
+
+	G.setBG();
+
+	// Change status line text
+
+	G.setStatusText();
+
+	// Set border visible
+
+	G.setBorderVisibility();
+
+	// Preload click sound
+
+	PS.audioLoad( "fx_drip2" );
 };
 
-// PS.touch ( x, y, data, options )
-// Called when the mouse button is clicked on a bead, or when a bead is touched
+PS.touch = function( x, y, data, options ) {
+	// Toggle color of touched bead from white to black and back again
+	// NOTE: The default value of a bead's [data] is 0, which happens to be equal to PS.COLOR_BLACK
 
-PS.touch = function ( x, y, data, options ) {
-	G.touch( x, y ); // initiates actor movement
+	PS.color( x, y, G.getColor() ); // set color to current color value in G
+
+	PS.audioPlay( "fx_drip2" );
 };
 
+/*
+PS.release ( x, y, data, options )
+Called when the left mouse button is released, or when a touch is lifted, over bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.release = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.release() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse button/touch is released over a bead.
+};
+
+/*
+PS.enter ( x, y, button, data, options )
+Called when the mouse cursor/touch enters bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.enter = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.enter() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch enters a bead.
+};
+
+/*
+PS.exit ( x, y, data, options )
+Called when the mouse cursor/touch exits bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exit = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.exit() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch exits a bead.
+};
+
+/*
+PS.exitGrid ( options )
+Called when the mouse cursor/touch exits the grid perimeter.
+This function doesn't have to do anything. Any value returned is ignored.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exitGrid = function( options ) {
+	// Uncomment the following code line to verify operation:
+
+	// PS.debug( "PS.exitGrid() called\n" );
+
+	// Add code here for when the mouse cursor/touch moves off the grid.
+};
+
+/*
+PS.keyDown ( key, shift, ctrl, options )
+Called when a key on the keyboard is pressed.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyDown = function( key, shift, ctrl, options ) {
+	// The size of the grid can be changed using the arrow keys
+	// left decreases width, right increases width
+	// down decreases height, up increases height
+
+	// The color placed when clicking can be changed with Q and W
+
+	// Border visibility can be toggled with B
+
+	//BG color can be toggled with C
+
+	let dimensions = PS.gridSize();
+
+	switch(key){
+		// grid size cases
+		case PS.KEY_ARROW_LEFT:
+			PS.gridSize(dimensions.width - 1, dimensions.height);
+			break;
+		case PS.KEY_ARROW_RIGHT:
+			PS.gridSize(dimensions.width + 1, dimensions.height);
+			break;
+		case PS.KEY_ARROW_DOWN:
+			PS.gridSize(dimensions.width, dimensions.height - 1);
+			break;
+		case PS.KEY_ARROW_UP:
+			PS.gridSize(dimensions.width, dimensions.height + 1);
+			break;
+
+		//color cases
+		case 113: //q
+			G.prevColor();
+			break;
+		case 119: //w
+			G.nextColor();
+			break;
+		
+		//border visibility cases
+		case 98: //b
+			G.toggleBorderVisibility();
+			break;
+		
+		//bg color cases
+		case 99: //c
+			G.toggleBG();
+			break;
+	}
+	G.setBG();
+	G.setBorderVisibility();
+};
+
+/*
+PS.keyUp ( key, shift, ctrl, options )
+Called when a key on the keyboard is released.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyUp = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyUp(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is released.
+};
+
+/*
+PS.input ( sensors, options )
+Called when a supported input device event (other than those above) is detected.
+This function doesn't have to do anything. Any value returned is ignored.
+[sensors : Object] = A JavaScript object with properties indicating sensor status; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+NOTE: Currently, only mouse wheel events are reported, and only when the mouse cursor is positioned directly over the grid.
+*/
+
+PS.input = function( sensors, options ) {
+	// Uncomment the following code lines to inspect first parameter:
+
+//	 var device = sensors.wheel; // check for scroll wheel
+//
+//	 if ( device ) {
+//	   PS.debug( "PS.input(): " + device + "\n" );
+//	 }
+
+	// Add code here for when an input event is detected.
+};
 
