d89b561211ed4d835ae6aef31208e1338bb01fde
npelletier2 <npelletier3435@gmail.com>
2022-04-26 08:16:59 -0400

    Revert "made changes on the wrong thing, going to revert this one"
    This reverts commit 736d067b5afe95877ebabe25ebeb7fe8390a1804.

--- 2900/A19/game.js
+++ 2900/A19/game.js
@@ -40,21 +40,13 @@ If you don't use JSHint (or are using it with a configuration file), you can saf
 
 const G = (function(){
 	// ------ Constants ------
-	const floor_plane = 0;
+
+	const player_plane = 5;
+	const player_color = 0x00DD99;
 	const wall_plane = 1;
+	const wall_color = 0x000000;
 	const door_plane = 2;
 	const egg_plane = 3;
-	const locked_door_plane = 4;
-	const player_plane = 5;
-
-	const player_color = 0xA1C93A;
-	const wall_color = 0x000000;
-	const ground_color_default = 0xFFFFFF;
-	let ground_color_curr = ground_color_default;
-	const slippy_color = 0x77eeff;
-	const locked_door_color = 0x333333;
-
-	const easter_egg_count = 3;
 
 	// ------ Rooms ------
 
@@ -62,7 +54,7 @@ const G = (function(){
 		start: {
 			size: {x:5, y:7},
 			layout: [
-				[1,1,4,1,1],
+				[1,1,1,1,1],
 				[1,0,2,0,1],
 				[1,0,0,0,1],
 				[1,0,0,0,1],
@@ -74,16 +66,12 @@ const G = (function(){
 				'0': {type: null},
 				'1': {type: 'wall'},
 				'2': {type: 'player'},
-				'3': {type: 'door', dir: {x:0,y:1}, goto: 'room1'},
-				'4': {type: 'locked_door'}
+				'3': {type: 'door', dir: {x:0,y:1}, goto: 'room1'}
 			},
 			onEnter: function(){
-				if(!player.has_key) printStatus("You're now locked in here. Excellent work.");
-				else printStatus("Unlock the door... or maybe explore more?")
+				PS.statusText("You're now locked in here. Excellent work.");
 			},
-			onExit: function(){
-				locked_door.unplace();
-			}
+			onExit: function(){}
 		},
 		room1: {
 			size: {x:9, y:10},
@@ -95,65 +83,31 @@ const G = (function(){
 				[4,0,0,0,0,0,0,0,1],
 				[1,0,0,0,0,0,0,0,1],
 				[1,0,0,0,0,0,0,0,1],
-				[1,0,0,0,0,0,0,0,2],
+				[1,0,0,0,0,0,0,0,1],
 				[1,0,0,0,0,0,0,0,1],
 				[1,1,5,1,1,1,1,1,1],
 			],
 			key: {
 				'0': {type: null},
 				'1': {type: 'wall'},
-				'2': {type: 'door', dir: {x:1,y:0}, goto: 'easter_egg'},
 				'3': {type: 'door', dir: {x:0,y:-1}, goto: 'start'},
 				'4': {type: 'door', dir: {x:-1,y:0}, goto: 'wester_egg'},
 				'5': {type: 'door', dir: {x:0,y:1}, goto: 'big_room'}
 			},
 			onEnter: function(){
-				printStatus("Maybe there's an easter egg with the key in it.");
-			},
-			onExit: function(){}
-		},
-		easter_egg: {
-			size: {x:16, y:6},
-			layout: [
-				[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1],
-				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-				[4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-				[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-			],
-			key: {
-				'0': {type: null},
-				'1': {type: 'wall'},
-				'2': {
-					type: 'egg', 
-					color: PS.COLOR_MAGENTA, 
-					pickup: function(){
-						printStatus("It doesn't have the key in it, though.");
-						rooms.easter_egg.key[2].picked_up = true;
-					},
-					picked_up: false
-				},
-				'4': {type: 'door', dir: {x:-1,y:0}, goto: 'room1'}
-			},
-			onEnter: function(){
-				if(!rooms.easter_egg.key[2].picked_up){
-					printStatus("Oh look, there's an easter egg!")
-				}else{
-					printStatus("");
-				}
+				PS.statusText("Maybe there's an easter egg with the key in it.");
 			},
 			onExit: function(){}
 		},
 		wester_egg: {
-			size: {x:16, y:6},
+			size: {x:7, y:6},
 			layout: [
-				[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4],
-				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-				[1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-				[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+				[1,1,1,1,1,1,1],
+				[1,0,0,0,0,0,4],
+				[1,0,0,0,0,0,1],
+				[1,0,0,0,0,0,1],
+				[1,0,2,0,0,0,1],
+				[1,1,1,1,1,1,1],
 			],
 			key: {
 				'0': {type: null},
@@ -162,10 +116,8 @@ const G = (function(){
 					type: 'egg', 
 					color: PS.COLOR_VIOLET, 
 					pickup: function(){
-						printStatus("Hmmm... Nope, that's a wester egg.");
+						PS.statusText("Hmmm... Nope, that's a wester egg.");
 						rooms.wester_egg.key[2].picked_up = true;
-						player.wester_egg = true;
-						player.egg_count--; //compensate for eggs automatically adding to the player count when picked up; this one shouldn't
 					},
 					picked_up: false
 				},
@@ -173,13 +125,9 @@ const G = (function(){
 			},
 			onEnter: function(){
 				if(!rooms.wester_egg.key[2].picked_up){
-					if(!rooms.easter_egg.key[2].picked_up){
-						printStatus("I think that's an easter egg over there.");
-					}else{
-						printStatus("Look, another easter egg!");
-					}
+					PS.statusText("Oh look, there's an easter egg!")
 				}else{
-					printStatus("Hmmm.");
+					PS.statusText("Hmmm.");
 				}
 			},
 			onExit: function(){}
@@ -188,19 +136,19 @@ const G = (function(){
 			size: {x:16,y:16},
 			layout: [
 				[1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,2],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
-				[1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1],
 				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-				[1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 				[1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1],
 			],
@@ -208,13 +156,13 @@ const G = (function(){
 				'0': {type: null},
 				'1': {type: 'wall'},
 				'2': {type: 'door', dir: {x:1,y:0}, goto: 'perlenspiel'},
-				'3': {type: 'door', dir: {x:0,y:1}, goto: 'slippy'},
+				'3': {type: 'door', dir: {x:0,y:1}, goto: 'proto_win_room'},
 				'5': {type: 'door', dir: {x:0,y:-1}, goto: 'room1'}
 			},
 			onEnter: function(){
-				printStatus("Woah, this is a big room...");
-				rooms.big_room.wait_id = waitTicks(60, function(){
-					printStatus("At least it's only 16x16.");
+				PS.statusText("Woah, this is a big room...");
+				rooms.big_room.wait_id = waitTicks(17, function(){
+					PS.statusText("At least it's only 16x16.");
 				})
 			},
 			onExit: function(){
@@ -241,109 +189,26 @@ const G = (function(){
 				'0': {type: null},
 				'1': {type: 'wall'},
 				'2': {type: 'door', dir: {x:-1,y:0}, goto: 'big_room'},
-				'3': {type: 'floor', color: 0x87578E, onWalked: function(){
-						PS.audioPlay("l_hchord_db4");
-					}},
-				'4': {type: 'floor', color: 0x6A98BA, onWalked: function(){
-						PS.audioPlay("l_hchord_d4");
-					}},
-				'5': {type: 'floor', color: 0xFFFFFF, onWalked: function(){
-						player.allow_movement = false;
-						PS.audioPlay("fx_click");
-						waitTicks(30, rooms.perlenspiel.playPerlenspiel);
-					},
-					allow_collision: false
-				},
-				'6': {type: 'floor', color: 0xF4782B, onWalked: function(){
-						PS.audioPlay("l_hchord_f4");
-					}},
-				'7': {type: 'floor', color: 0xEFEB42, onWalked: function(){
-						PS.audioPlay("l_hchord_a4");
-					}}
+				'3': {type: 'egg', color: 0x87578E, pickup: function(){
+					PS.audioPlay("l_hchord_db4");
+				}},
+				'4': {type: 'egg', color: 0x6A98BA, pickup: function(){
+					PS.audioPlay("l_hchord_d4");
+				}},
+				'5': {type: 'egg', color: 0xA1C93A, pickup: function(){
+					PS.audioPlay("l_hchord_e4");
+				}},
+				'6': {type: 'egg', color: 0xF4782B, pickup: function(){
+					PS.audioPlay("l_hchord_f4");
+				}},
+				'7': {type: 'egg', color: 0xEFEB42, pickup: function(){
+					PS.audioPlay("l_hchord_a4");
+				}}
 			},
 			onEnter: function(){
-				if(rooms.perlenspiel.room_complete){
-					printStatus("Ah, now the room looks better.");
-				}else{
-					printStatus("What do those buttons do?");
-				}
+				PS.statusText("That's a lot of eggs!");
 			},
-			onExit: function(){},
-			playPerlenspiel: function(){
-				// PS.fade(PS.ALL, PS.ALL, 20);
-				rooms.perlenspiel.playPerlenspiel.playNote = function(){
-					if(rooms.perlenspiel.playPerlenspiel.playNote.index === undefined){//uninitialized
-						rooms.perlenspiel.playPerlenspiel.playNote.index = 0;
-						rooms.perlenspiel.playPerlenspiel.playNote.melody = 
-						["l_hchord_d4", "", "l_hchord_a4", "", "l_hchord_f4", "", "l_hchord_d4", 
-						"", "l_hchord_db4", "", "hchord_d4", "hchord_e4", "l_hchord_f4"];
-					}
-					
-					//play note
-					const melody = rooms.perlenspiel.playPerlenspiel.playNote.melody;
-					let index = rooms.perlenspiel.playPerlenspiel.playNote.index;
-					PS.audioPlay(melody[index]);
-					
-					//flash tile
-					if(melody[index] !== ""){
-						const curr_plane = PS.gridPlane();
-						PS.gridPlane(10);
-
-						//find position of leftmost tile that has not been flashed yet
-						let pos = PS.spriteMove(perlenspiel_tiles[0]);
-						let pos_index = 0;
-						for(let i = 1; i < perlenspiel_tiles.length; i++){
-							const curr_pos = PS.spriteMove(perlenspiel_tiles[i]);
-							if(curr_pos.x < pos.x){
-								pos = curr_pos;
-								pos_index = i;
-							}
-						}
-
-						PS.color(pos.x, pos.y, 0xFFFFFF);
-						PS.fade(pos.x, pos.y, 0);
-						PS.alpha(pos.x, pos.y, 255);
-						PS.fade(pos.x, pos.y, 20);
-						PS.alpha(pos.x, pos.y, 0);
-
-						perlenspiel_tiles.splice(pos_index, 1);
-						PS.gridPlane(curr_plane);
-					}
-
-					//play next note, or end
-					rooms.perlenspiel.playPerlenspiel.playNote.index++;
-					if(rooms.perlenspiel.playPerlenspiel.playNote.index >= melody.length){//end
-						waitTicks(30, function(){
-							for(const sprite of Object.getOwnPropertyNames(floor_ids)){
-								PS.spriteDelete(sprite);
-								delete floor_ids[sprite];
-							}
-							perlenspiel_tiles = [];
-							new Egg(5, 5, rooms.perlenspiel.reward_egg);
-							printStatus("An easter egg appeared!");
-							waitTicks(20, function(){
-								PS.fade(PS.ALL, PS.ALL, 0);
-								player.allow_movement = true;
-							});
-						})
-					}else{
-						waitTicks(10, rooms.perlenspiel.playPerlenspiel.playNote);
-					}
-				}
-				rooms.perlenspiel.playPerlenspiel.playNote();
-			},
-			reward_egg: {
-				type: 'egg', 
-				color: PS.COLOR_CYAN, 
-				pickup: function(){
-					rooms.perlenspiel.room_complete = true;
-					rooms.perlenspiel.key['5'] = {type: 'floor', color: 0xA1C93A, onWalked: function(){
-						PS.audioPlay("l_hchord_e4");
-					}};
-				},
-				picked_up: false
-			},
-			room_complete: false
+			onExit: function(){}
 		},
 		proto_win_room: {
 			size: {x:7, y:9},
@@ -361,56 +226,17 @@ const G = (function(){
 			key:{
 				'0': {type: null},
 				'1': {type: 'wall'},
-				'3': {type: 'door', dir: {x:0,y:-1}, goto: 'slippy'},
+				'3': {type: 'door', dir: {x:0,y:-1}, goto: 'big_room'},
 				'4': {type: 'egg', color: 0xffcc00, pickup: function(){
-					printStatus("You found the key!");
+					PS.statusText("You found the key! It does nothing for now.");
 					rooms.proto_win_room.key[4].picked_up = true;
-					player.has_key = true;
 				}, picked_up: false}
 			},
 			onEnter: function(){
-				if(!rooms.proto_win_room.key[4].picked_up) printStatus("A golden egg?!");
-				else printStatus("You found the key!");
+				if(!rooms.proto_win_room.key[4].picked_up) PS.statusText("Pick up the egg to win(?)");
+				else PS.statusText("You found the key! It does nothing for now.");
 			},
 			onExit: function(){}
-		},
-		slippy: {
-			size: {x:10, y:10},
-			layout: [
-				[1,1,1,3,1,1,1,1,1,1],
-				[1,0,1,0,0,0,0,0,0,1],
-				[1,0,1,0,0,0,0,1,0,1],
-				[1,0,0,0,0,0,0,1,0,1],
-				[1,0,0,0,0,0,0,0,0,1],
-				[1,0,0,0,0,0,0,0,0,1],
-				[1,0,0,0,0,0,0,0,1,1],
-				[1,0,0,0,0,0,1,0,1,1],
-				[1,0,0,1,0,0,1,0,1,1],
-				[1,1,1,1,1,1,1,4,1,1],
-			],
-			key:{
-				'0': {type: null},
-				'1': {type: 'wall'},
-				'3': {type: 'door', dir: {x:0,y:-1}, goto: 'big_room'},
-				'4': {type: 'door', dir: {x:0,y:1}, goto: 'proto_win_room'},
-			},
-			prePlace: function(){
-				ground_color_curr = slippy_color;
-			},
-			onEnter: function(){
-				printStatus("The floor here is... uhm... what's the word...");
-				rooms.slippy.wait_id = waitTicks(60, function(){
-					printStatus("Oh, right! The floor here is really slippy!");
-				});
-				player.slippy_controls = true;
-			},
-			onExit: function(){
-				cancelWait(rooms.slippy.wait_id);
-				rooms.slippy.wait_id = -1;
-				ground_color_curr = ground_color_default;
-				player.slippy_controls = false;
-			},
-			wait_id: -1
 		}
 	};
 
@@ -419,7 +245,7 @@ const G = (function(){
 	//called on game startup
 	const init = function(){
 		loadRoom('start');
-		PS.timerStart(2, G.update);
+		PS.timerStart(6, G.update);
 	};
 
 	//called whenever a new room is loaded. initializes a new grid with predefined formatting
@@ -461,9 +287,6 @@ const G = (function(){
 				controls.interact = is_down;
 				break;
 		}
-		if(is_down){
-			player.keyPressed(key);
-		}
 	};
 
 	//called every tick
@@ -476,9 +299,6 @@ const G = (function(){
 
 		//update player
 		player.update();
-
-		//update status text
-		updateStatus();
 	};
 
 	const wait_queue = {};
@@ -527,21 +347,12 @@ const G = (function(){
 			PS.spriteDelete(sprite);
 			delete door_ids[sprite];
 		}
-		for(const sprite of Object.getOwnPropertyNames(floor_ids)){
-			PS.spriteDelete(sprite);
-			delete floor_ids[sprite];
-		}
 
 		//get room to be loaded
 		const room = rooms[room_name];
 
 		//initialize grid
 		gridInit(room.size.x, room.size.y);
-
-		//call prePlace for room being loaded
-		if(room.prePlace !== undefined){
-			room.prePlace();
-		}
 
 		//place objects
 		for(let y = 0; y < room.size.y; y++){
@@ -563,7 +374,6 @@ const G = (function(){
 			}
 		}
 
-		//call onEnter for room being loaded
 		if(room.onEnter !== undefined){
 			room.onEnter();
 		}
@@ -573,9 +383,6 @@ const G = (function(){
 
 	const place = function(obj, x, y){
 		switch(obj.type){
-			case null:
-				PS.color(x,y,ground_color_curr);
-				break;
 			case 'player':
 				player.init(x,y);
 				break;
@@ -588,12 +395,6 @@ const G = (function(){
 			case 'egg':
 				if(!obj.picked_up) new Egg(x,y,obj);
 				break;
-			case 'floor':
-				new Floor(x,y,obj);
-				break;
-			case 'locked_door':
-				locked_door.place(x,y);
-				break;
 		}
 	}
 
@@ -604,39 +405,17 @@ const G = (function(){
 		}
 	}
 
-	//prints a string to the status bar one character at a time
-	let print_queue = [];
-	const printStatus = function(str){
-		print_queue[0] = str;
-		print_queue[1] = "";
-	}
-	const updateStatus = function(){
-		if(print_queue.length !== 0){
-			const index = print_queue[1].length;
-			print_queue[1] += print_queue[0].charAt(index)
-			PS.statusText(print_queue[1]);
-			if(print_queue[1].length === print_queue[0].length){
-				print_queue = [];
-			}
-		}
-	}
-
 	// ------ Objects ------
 
 	const player = {
 		sprite: undefined,
 		direction: {x:0,y:0},
 		door_collision: false,
-		allow_movement: true,
-		has_key: false,
-		egg_count: 0,
-		wester_egg: false,
 		init: function(x,y){
 			this.collide = this.collide.bind(this);
 			this.getInput = this.getInput.bind(this);
 			this.move = this.move.bind(this);
 			this.update = this.update.bind(this);
-			this.keyPressed = this.keyPressed.bind(this);
 
 			this.sprite = PS.spriteSolid(1,1);
 			PS.spritePlane(this.sprite, player_plane);
@@ -659,9 +438,9 @@ const G = (function(){
 						s1 = s2;
 					}
 					const door = door_ids[s1];
-					//if(this.direction.x === door.direction.x && this.direction.y === door.direction.y){
+					if(this.direction.x === door.direction.x && this.direction.y === door.direction.y){
 						load_room_next_tick = door.goto;
-					//}
+					}
 				}
 				if(p1 === egg_plane || p2 === egg_plane){//collide with egg
 					if(p2 === egg_plane){
@@ -671,23 +450,6 @@ const G = (function(){
 					egg.pickup();
 					markDelete(egg.sprite);
 					delete egg_ids[egg.sprite];
-				}
-				if(p1 === floor_plane || p2 === floor_plane){
-					if(p2 === floor_plane){
-						s1 = s2;
-					}
-					const floor = floor_ids[s1];
-					floor.onWalked();
-				}
-				if(p1 === locked_door_plane || p2 === locked_door_plane){
-					if(player.has_key){
-						locked_door.unlock();
-					}
-					this.direction.x = -this.direction.x;
-					this.direction.y = -this.direction.y;
-					this.move();
-					this.direction.x = 0;
-					this.direction.y = 0;
 				}
 			}
 		},
@@ -732,69 +494,10 @@ const G = (function(){
 				loadRoom(load_room_next_tick);
 				load_room_next_tick = null;
 			}
-			this.movement_count++;
-			if(this.allow_movement && this.movement_count >= this.movement_slowdown){
-				this.movement_count = 0;
-				if(!this.slippy_controls) this.getInput();
-				this.move();
-			}
-		},
-		movement_slowdown: 3,
-		movement_count: 0,
-		keyPressed: function(key){
-			if(this.slippy_controls && this.direction.x === 0 && this.direction.y === 0){
-				switch(key){
-					case 119: //w
-					case PS.KEY_ARROW_UP:
-						this.direction.x = 0;
-						this.direction.y = -1;
-						break;
-					case 97: //a
-					case PS.KEY_ARROW_LEFT:
-						this.direction.x = -1;
-						this.direction.y = 0;
-						break;
-					case 115: //s
-					case PS.KEY_ARROW_DOWN:
-						this.direction.x = 0;
-						this.direction.y = 1;
-						break;
-					case 100: //d
-					case PS.KEY_ARROW_RIGHT:
-						this.direction.x = 1;
-						this.direction.y = 0;
-						break;
-				}
-			}
-		},
-		slippy_controls: false
-	};
-
-	const locked_door = {
-		locked: true,
-		sprite: undefined,
-		place: function(x, y){
-			if(this.locked){
-				this.sprite = PS.spriteSolid(1,1);
-				PS.spritePlane(this.sprite, locked_door_plane);
-				PS.spriteMove(this.sprite, x, y);
-				PS.spriteSolidColor(this.sprite, locked_door_color);
-				PS.spriteSolidAlpha(this.sprite, 255);
-			}
-		},
-		unplace: function(){
-			if(this.sprite !== undefined){
-				markDelete(this.sprite);
-				this.sprite = undefined;
-			}
-		},
-		unlock: function(){
-			this.locked = false;
-			this.unplace();
-			player.allow_movement = false;
-			printStatus("You win! Easter eggs: " + player.egg_count + "/" + easter_egg_count + (player.wester_egg ? " (and a wester egg)" : ""));
+			this.getInput();
+			this.move();
 		}
-	}
+	};
 
 	class Wall{
 		constructor(x, y){
@@ -832,35 +535,15 @@ const G = (function(){
 			PS.spriteSolidColor(this.sprite, obj.color);
 			PS.spriteSolidAlpha(this.sprite, 255);
 
-			this.pickup = function(){
-				obj.pickup();
-				player.egg_count++;
-			}
+			this.pickup = obj.pickup;
 
 			egg_ids[this.sprite] = this;
 		}
 	}
 	let egg_ids = {};
 
-	class Floor{
-		constructor(x, y, obj){
-			this.sprite = PS.spriteSolid(1,1);
-			PS.spritePlane(this.sprite, floor_plane);
-			PS.spriteMove(this.sprite, x, y);
-			PS.spriteSolidColor(this.sprite, obj.color);
-			PS.spriteSolidAlpha(this.sprite, 255);
-
-			this.onWalked = obj.onWalked;
-
-			floor_ids[this.sprite] = this;
-			perlenspiel_tiles.push(this.sprite);
-		}
-	}
-	let floor_ids = {};
-	let perlenspiel_tiles = [];
-
 	return {
-		update, handleInput, init, player
+		update, handleInput, init
 	};
 }())
 
